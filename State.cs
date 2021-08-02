af3cba0ab9d62049e0be030f65baca070fad5664
Kevin Bernard <kevos.directos@gmail.com>
2021-08-02 13:08:20 +0200

    Added starting state with first dialog being fetched at game start

--- State.cs
+++ State.cs
@@ -5,5 +5,11 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
 {
-    [TextArea(10,14)] [SerializeField] string StoryText;
+    [TextArea(10, 14)] [SerializeField] string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
+    
 }
